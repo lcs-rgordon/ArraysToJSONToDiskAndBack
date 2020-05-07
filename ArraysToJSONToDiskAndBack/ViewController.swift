8bc45342f1540fd7036747e02ae297cdd76ff802
Russell Gordon <rgordon@lcs.on.ca>
2020-05-07 12:56:46 -0400

    Set up basic view for app.

--- ArraysToJSONToDiskAndBack/ViewController.swift
+++ ArraysToJSONToDiskAndBack/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldFirstName1: UITextField!
+    @IBOutlet weak var textFieldLastName1: UITextField!
+    @IBOutlet weak var textFieldFirstName2: UITextField!
+    @IBOutlet weak var textFieldLastName2: UITextField!
+    @IBOutlet weak var textFieldFirstName3: UITextField!
+    @IBOutlet weak var textFieldLastName3: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func saveNames(_ sender: Any) {
+        
+    }
+    
 }
 
